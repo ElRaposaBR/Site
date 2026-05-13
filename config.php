6fd5b0285ed35014a5b3579022d26e81b6699bb6
ElRaposaBR <91401531+ElRaposaBR@users.noreply.github.com>
2026-05-13 19:16:48 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$conn = new mysqli("sql10.freesqldatabase.com", "sql10826736", "sql10826736", "sql10826736");
+$conn = new mysqli("sql10.freesqldatabase.com", "sql10826736", "mvww2twiVW", "sql10826736");
 
 if ($conn->connect_error) {
   die("Erro conexão");
